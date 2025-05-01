7a0b3c91ce7b9a9dbc3709ac4208585eb1a1c34d
cfonbuena <cfonbuena@students.avc.edu>
2025-05-01 12:20:48 -0700

    add: ex1 code (has syntax err)

---
+++ ex1.asm
@@ -0,0 +1,31 @@
+.data
+    g: .word 100
+    h: .word 200
+    ival: .word 10
+    jval: .word 20
+    fval: .word 0
+
+.text
+.globl main
+main:
+    lw $t1, g
+    lw $t2, h
+    lw $t3, ival
+    lw $t4, jval
+
+    bne $t3, $t4, false
+    add $t0, $t1, $t2
+    j endif
+
+false:
+    sub $t0, $t1, $t2
+
+endif: 
+    sw $t0, fval
+
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    li $v0, 10
+    syscall
