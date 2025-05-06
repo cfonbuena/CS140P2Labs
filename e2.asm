6d0695c432fc8ad62cff5b38419508c6ea75a947
cfonbuena <cfonbuena@students.avc.edu>
2025-05-06 11:32:48 -0700

    add: ex2 code (from lecture)

---
+++ e2.asm
@@ -0,0 +1,37 @@
+.data
+    i: .word 100
+    jval: .word 100
+    f: .word 0
+    g: .word 10
+    h: .word 5
+
+.text
+.globl main
+main:
+    lw $t0, f
+    lw $t1, g
+    lw $t2, h
+    lw $t3, i
+    lw $t4, jval
+
+    slt $t5, $t3, $t4
+    beq $t5, $zero, false
+    add $t0, $t1, $t2
+    j exit 
+
+false:
+    lui $t5, 0xFFFF
+    ori $t5, $t5, 0xFFFF
+    xor $t2, $t2, $t5
+    addi $t2, $t2, 1
+    add $t0, $t1, $t2
+
+exit:
+    sw $t0, f
+
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    li $v0, 10
+    syscall
